6a1f9aa7b8b79544d25c279780a4fc1e4cf0cd7a
Karim <127413175+pywolf1503@users.noreply.github.com>
2024-11-08 13:29:01 +0000

    Removed Slide checks inside of SlideApp

--- components/ui/slides/SlideApp.tsx
+++ components/ui/slides/SlideApp.tsx
@@ -1,17 +1,8 @@
 "use client";
 import { ReactChildren } from "@/types/common";
 import { SlideAppProvider } from "./SlideAppContext";
-import React, { isValidElement, useEffect } from "react";
-import { SLIDE_OWNER_NAME } from "@/lib/constants";
+import React from "react";
 
 export const SlideApp: React.FC<ReactChildren> = ({ children }) => {
-  useEffect(() => {
-    React.Children.toArray(children).forEach((child) => {
-      //@ts-expect-error : _owner is a hidden React property!
-      if(!(isValidElement(child) && child._owner.name == SLIDE_OWNER_NAME)){
-        throw new Error("SlideApp accepts only Slide components!");
-      }
-    });
-  }, [children]);
   return <SlideAppProvider>{children}</SlideAppProvider>;
 };
